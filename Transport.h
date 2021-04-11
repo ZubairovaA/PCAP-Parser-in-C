2bd6c7446c234d14e63da843de8cca61b1955396
ZubairovaA <41435166+ZubairovaA@users.noreply.github.com>
2021-04-11 15:31:28 +1000

    Update Transport.h

--- Transport.h
+++ Transport.h
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-struct Transport_tcp {           //структура заголовка TCP    TCP  header struct
+struct Transport_tcp {           //Г±ГІГ°ГіГЄГІГіГ°Г  Г§Г ГЈГ®Г«Г®ГўГЄГ  TCP    TCP  header struct
 
-    unsigned short src_port;	// номер порта отправителя                 source port
-    unsigned short dst_port;	// номер порта получателя                  destination port
-    unsigned long th_seq;		// номер пакета в последовательности       sequence number
-    unsigned long th_ack;		// номер подтверждения                     acknowledgement number
-    unsigned char th_offx2;	    // длина заголовка (4 бита) TCP_Length = (tcp->th_offx2) >>4     data offset
-    unsigned char th_flags;     //флаги    flags
+    unsigned short src_port;	// Г­Г®Г¬ГҐГ° ГЇГ®Г°ГІГ  Г®ГІГЇГ°Г ГўГЁГІГҐГ«Гї                 source port
+    unsigned short dst_port;	// Г­Г®Г¬ГҐГ° ГЇГ®Г°ГІГ  ГЇГ®Г«ГіГ·Г ГІГҐГ«Гї                  destination port
+    unsigned long th_seq;		// Г­Г®Г¬ГҐГ° ГЇГ ГЄГҐГІГ  Гў ГЇГ®Г±Г«ГҐГ¤Г®ГўГ ГІГҐГ«ГјГ­Г®Г±ГІГЁ       sequence number
+    unsigned long th_ack;		// Г­Г®Г¬ГҐГ° ГЇГ®Г¤ГІГўГҐГ°Г¦Г¤ГҐГ­ГЁГї                     acknowledgement number
+    unsigned char th_offx2;	    // Г¤Г«ГЁГ­Г  Г§Г ГЈГ®Г«Г®ГўГЄГ  (4 ГЎГЁГІГ ) TCP_Length = (tcp->th_offx2) >>4     data offset
+    unsigned char th_flags;     //ГґГ«Г ГЈГЁ    flags
 #define TH_FIN 0x01
 #define TH_SYN 0x02
 #define TH_RST 0x04
@@ -23,17 +23,19 @@ struct Transport_tcp {           //структура заголовка TCP    TCP  header struct
 #define TH_ECE 0x40
 #define TH_CWR 0x80
 #define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-    unsigned short th_win;		//окно                      window
-    unsigned short sum;		    //чексумма                  checksum
-    unsigned short th_urp;		//экстренный указатель      urgent pointer  
+    unsigned short th_win;		//Г®ГЄГ­Г®                      window
+    unsigned short sum;		    //Г·ГҐГЄГ±ГіГ¬Г¬Г                   checksum
+    unsigned short th_urp;		//ГЅГЄГ±ГІГ°ГҐГ­Г­Г»Г© ГіГЄГ Г§Г ГІГҐГ«Гј      urgent pointer  
 };
 
 
 struct Handshake {
-    unsigned short src_port;	// номер порта отправителя                 source port
-    unsigned short dst_port;	// номер порта получателя                  destination portint 
+    unsigned short src_port;	// РЅРѕРјРµСЂ РїРѕСЂС‚Р° РѕС‚РїСЂР°РІРёС‚РµР»СЏ                 source port
+    unsigned short dst_port;	// РЅРѕРјРµСЂ РїРѕСЂС‚Р° РїРѕР»СѓС‡Р°С‚РµР»СЏ                  destination portint 
+    unsigned long sequence_number=0; 
     int Contact = 0;    
     int Finish = 0;
+    unsigned long Ack_number = 0;
 };
 
 void Handle_TCP(Transport_tcp& TCP, vector <Handshake>& Sessions, int& Handshakes_Sucsess);   //Checking if there are any handshakes in the file
