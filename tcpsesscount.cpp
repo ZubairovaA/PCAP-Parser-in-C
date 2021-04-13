5af0a5fe5f5834297dfd2d274db4993fbd9df185
ZubairovaA <41435166+ZubairovaA@users.noreply.github.com>
2021-04-13 23:08:22 +1000

    Update tcpsesscount.cpp

--- tcpsesscount.cpp
+++ tcpsesscount.cpp
@@ -1,4 +1,5 @@
-// tcpsesscount.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// TCP_Sessions.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
 
 
 #include <iostream>
@@ -9,16 +10,14 @@
 #pragma comment(lib, "ws2_32.lib")
 #include <stdio.h>
 #include <malloc.h>
-#include"PCAP_Headers.h"
-#include"Link.h"
-#include"Internet.h"
-#include"Transport.h"
+#include "Headers.h"
+#include"Handshake.h"
 
 using namespace std;
 
-void Parse(FILE* ptrFile, Link& ethernet, pcap_pkthdr& ptk_header, Internet_ip& ip, long & pkt_offset, Transport_tcp& TCP, Handshake* Sessions, int & Handshakes_Sucsess, int& index, int& Array_Size)
+void Parse(FILE* ptrFile, Link& ethernet, pcap_pkthdr& ptk_header, Internet_ip& ip, long& pkt_offset, Transport_tcp& TCP, Handshake* Sessions, int& Handshakes_Sucsess, int& index, int& Array_Size)
 {
-    
+
     while (fseek(ptrFile, pkt_offset, SEEK_SET) == 0)
     {
         if (fread(&ptk_header, 16, 1, ptrFile) == 0) //read pcap packet header 16 bytes
@@ -35,7 +34,7 @@ void Parse(FILE* ptrFile, Link& ethernet, pcap_pkthdr& ptk_header, Internet_ip& 
         }
 
         bool Continue = Handle_Ethernet(ethernet, ptrFile);
-        if (Continue == true) 
+        if (Continue == true)
         {
             continue;
         }
@@ -46,7 +45,7 @@ void Parse(FILE* ptrFile, Link& ethernet, pcap_pkthdr& ptk_header, Internet_ip& 
 
             break;
         }
-        if (ip.ip_p == IPPROTO_TCP) 
+        if (ip.ip_p == IPPROTO_TCP)
         {
             if (fread(&TCP, sizeof(TCP), 1, ptrFile) == 0) //read pcap packet head
             {
@@ -60,35 +59,35 @@ void Parse(FILE* ptrFile, Link& ethernet, pcap_pkthdr& ptk_header, Internet_ip& 
         pkt_offset += 16 + ptk_header.caplen;
     }
 }
-    
 
-    int Unfinished (Handshake* Sessions, int& index)
-    {
-        int count = 0;
-        for (int i = 0; i < index; i++)
-        {
-            if (Sessions[i].Contact != 3)
-            {
-                count++;
-            }
-        }
-        return count;
-    }
 
-    int Unstandart_Finished(Handshake* Sessions, int& index)
+int Unfinished(Handshake* Sessions, int& index)
+{
+    int count = 0;
+    for (int i = 0; i < index; i++)
     {
-        int count = 0;
-        for (int i = 0; i < index; i++)
+        if (Sessions[i].Contact != 3)
         {
-            if(Sessions[i].Finish!=2)
             count++;
         }
-        return count;
     }
+    return count;
+}
+
+int Unstandart_Finished(Handshake* Sessions, int& index)
+{
+    int count = 0;
+    for (int i = 0; i < index; i++)
+    {
+        if (Sessions[i].Finish != 2)
+            count++;
+    }
+    return count;
+}
 
 int main()
 {
-    Link ethernet ;           //the struct of the Ethernet protocol header
+    Link ethernet;           //the struct of the Ethernet protocol header
     pcap_pkthdr ptk_header;   // the struct of the packet header
     Internet_ip ip;           //the struct of the IP protocol header
     Transport_tcp TCP;        //the struct of the TCP protocol header
@@ -99,7 +98,7 @@ int main()
     int Unfinished_Sessions = 0;
     int Unstarnadt_Sessions = 0;
     long pkt_offset = 24;           // the offset
-    int index = 0, Array_Size=1000;
+    int index = 0, Array_Size = 1000;
     Handshake* Sessions = (Handshake*)malloc(Array_Size * sizeof(Handshake));    // started hanshakes
     if (Sessions == NULL)
     {
@@ -110,42 +109,42 @@ int main()
     if (err == 0)
     {
         Parse(ptrFile, ethernet, ptk_header, ip, pkt_offset, TCP, Sessions, Handshakes_Sucsess, index, Array_Size);
-        if (index > 0)      
+        if (index > 0)
         {
             Unfinished_Sessions = Unfinished(Sessions, index);
             Unstarnadt_Sessions = Unstandart_Finished(Sessions, index);
-            
-            printf ("The.pcap file includes: \n");
+
+            printf("The.pcap file includes: \n");
             printf("Handshakes: %d.\n", Handshakes_Sucsess);
             printf("Unfinished sessions: %d.\n", Unfinished_Sessions);
             printf("Unstarnadt sessions: %d.\n", Unstarnadt_Sessions);
-        }  
-        else 
-        {
-            printf ("There are no handshakes in the .pcap file.\n");
-         
         }
-        
-        
+        else
+        {
+            printf("There are no handshakes in the .pcap file.\n");
+
+        }
+
+
         free(Sessions);
         err = fclose(ptrFile);
     }
-    else 
+    else
     {
-        printf ("Mistake\n");
+        printf("Mistake\n");
     }
 
 
-    
-     if( ptrFile)
-   {
-      err = fclose( ptrFile );
-      if ( err == 0 )
-      {
-         printf( "The file 'crt_fopen_s.c' was closed\n" );
-      }
-   }
-   
+
+    if (ptrFile)
+    {
+        err = fclose(ptrFile);
+        if (err == 0)
+        {
+            printf("The file 'crt_fopen_s.c' was closed\n");
+        }
+    }
+
 
     return 0;
 }
